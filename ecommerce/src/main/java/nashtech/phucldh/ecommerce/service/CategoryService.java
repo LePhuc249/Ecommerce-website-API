5cf4526fd50a8fa1d4032340a2309952595183f0
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-04 20:40:41 +0700

    Create Coupons Service and Category Service
    Create Serivce for coupons and category with method

---
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/service/CategoryService.java
@@ -0,0 +1,10 @@
+package nashtech.phucldh.ecommerce.service;
+
+import java.util.List;
+
+import nashtech.phucldh.ecommerce.entity.Category;
+
+public interface CategoryService {
+	
+	public List<Category> findAll();
+}
---
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/service/CouponsService.java
@@ -0,0 +1,18 @@
+package nashtech.phucldh.ecommerce.service;
+
+import java.util.List;
+
+import nashtech.phucldh.ecommerce.entity.Coupons;
+
+public interface CouponsService {
+	
+	public List<Coupons> findAll();
+	
+	public List<Coupons> findByItem();
+	
+	public Coupons getCouponByCode(String code);
+	
+	public void saveCoupon(Coupons theCoupon);
+	
+	public void deleteCouponById(String idCoupon);
+}
