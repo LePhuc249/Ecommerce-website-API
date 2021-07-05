1f96d13db512c9675c8538f4daab7e261e154be5
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-05 20:50:26 +0700

    Update AccountReponsitory.java

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/AccountReponsitory.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/AccountReponsitory.java
@@ -1,11 +1,15 @@
 package nashtech.phucldh.ecommerce.reponsitory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import nashtech.phucldh.ecommerce.entity.Account;
 
 @Repository
 public interface AccountReponsitory extends JpaRepository<Account, String> {
-
+	
+	@Query("select u from User u where u.username = :username or u.password = :password")
+	Account checkLogin(@Param("username") String username, @Param("password") String password);
 }
