838b70899baba208a28645a03cb6a9053492a9e1
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-06 21:31:54 +0700

    Add rest controller for account

---
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/restcontroller/AccountRestExceptionHandler.java
@@ -0,0 +1,31 @@
+package nashtech.phucldh.ecommerce.restcontroller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import nashtech.phucldh.ecommerce.error.AccountErrorResponse;
+import nashtech.phucldh.ecommerce.exception.AccountNotFoundException;
+
+@ControllerAdvice
+public class AccountRestExceptionHandler {
+	
+	@ExceptionHandler
+	public ResponseEntity<AccountErrorResponse> handleException(AccountNotFoundException exc){
+		AccountErrorResponse error = new AccountErrorResponse();
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<AccountErrorResponse> handleException(Exception exc){
+		AccountErrorResponse error = new AccountErrorResponse();
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+}
