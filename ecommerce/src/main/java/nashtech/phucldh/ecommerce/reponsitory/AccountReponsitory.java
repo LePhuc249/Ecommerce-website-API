4c1a05e2cb85fb458b5b00cb8e33031e8f0b86ca
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-06 21:27:53 +0700

    Update Account Repository
    Add new function to account repository

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/AccountReponsitory.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/AccountReponsitory.java
@@ -1,15 +1,25 @@
 package nashtech.phucldh.ecommerce.reponsitory;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import nashtech.phucldh.ecommerce.entity.Account;
 
 @Repository
 public interface AccountReponsitory extends JpaRepository<Account, String> {
+
+//	@Query("select u from User u where u.username = :username or u.password = :password")
+//	Account checkLogin(@Param("username") String username, @Param("password") String password);
+
+	Optional<Account> findByUsername(String username);
 	
-	@Query("select u from User u where u.username = :username or u.password = :password")
-	Account checkLogin(@Param("username") String username, @Param("password") String password);
+	Optional<Account> findByEmail(String email);
+	
+	Boolean existsByUsername(String username);
+
+	Boolean existsByEmail(String email);
 }
