2460534834533047b01524a0db017cfa76cc67c1
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-06 21:28:09 +0700

    Update Account Service

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/service/AccountService.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/service/AccountService.java
@@ -1,16 +1,22 @@
 package nashtech.phucldh.ecommerce.service;
 
-import nashtech.phucldh.ecommerce.entity.Account;
+import org.springframework.http.ResponseEntity;
+
+//import nashtech.phucldh.ecommerce.entity.Account;
+import nashtech.phucldh.ecommerce.payload.request.LoginRequest;
+import nashtech.phucldh.ecommerce.payload.request.SignUpRequest;
 
 public interface AccountService {
+	
+	public ResponseEntity<?> authenticateAccount(LoginRequest loginRequest);
 
-	public Account checkLogin(String username, String password);
+    public ResponseEntity<?> registerAccount(SignUpRequest signUpRequest);
 
-	public Account getAccountByEmail(String email);
-
-	public String getRoleByEmail(String email);
-
-	public void saveAccount(Account account);
-
-	public void deleteAccount(String username);
+//	public Account getAccountByEmail(String email);
+//
+//	public String getRoleByEmail(String email);
+//
+//	public void saveAccount(Account account);
+//
+//	public void deleteAccount(String username);
 }
