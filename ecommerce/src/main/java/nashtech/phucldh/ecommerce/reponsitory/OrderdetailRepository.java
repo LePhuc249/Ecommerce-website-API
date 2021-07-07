919120cd1e0d1b100e8cc9d7d202f43f8425cc88
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-07 11:18:43 +0700

    Update OrderdetailRepository.java

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/OrderdetailRepository.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/OrderdetailRepository.java
@@ -1,6 +1,10 @@
 package nashtech.phucldh.ecommerce.reponsitory;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import nashtech.phucldh.ecommerce.entity.Orderdetail;
@@ -8,4 +12,7 @@ import nashtech.phucldh.ecommerce.entity.Orderdetail;
 @Repository
 public interface OrderdetailRepository extends JpaRepository<Orderdetail, String> {
 
+	
+	@Query("select u.itemproperty from orderdetail u where u.orderid = :orderid")
+	List<String> getListItemProperty(@Param("orderid") String orderid);
 }
