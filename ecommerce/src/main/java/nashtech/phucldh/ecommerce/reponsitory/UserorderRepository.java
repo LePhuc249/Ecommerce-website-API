f729fe8c969900db6e0233fb385fe6ad424fbb42
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-08 09:12:40 +0700

    Update UserorderRepository.java

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/UserorderRepository.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/UserorderRepository.java
@@ -1,11 +1,28 @@
 package nashtech.phucldh.ecommerce.reponsitory;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import nashtech.phucldh.ecommerce.entity.Userorder;
 
 @Repository
 public interface UserorderRepository extends JpaRepository<Userorder, String>{
-
+	
+	List<Userorder> findByCustomerid(String customerid);
+	
+	@Query("select u.couponid from userorder u")
+	List<String> findByCouponid();
+	
+	@Query("update userorder SET status='Fi' where orderid=:orderid")
+	Boolean updateStatusToFinish(@Param("orderid")String orderID);
+	
+	@Query("update userorder SET status='Co' where orderid=:orderid")
+	Boolean updateStatusToConfirm(@Param("orderid")String orderID);
+	
+	@Query("update userorder SET status='Ca' where orderid=:orderid")
+	Boolean updateStatusToCancel(@Param("orderid")String orderID);
 }
