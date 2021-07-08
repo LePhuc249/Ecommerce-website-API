596524f6ff72a7022d87f969968ce09bd2205f17
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-08 09:12:32 +0700

    Update RelationshipCategoryRepository.java

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/RelationshipCategoryRepository.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/reponsitory/RelationshipCategoryRepository.java
@@ -1,5 +1,7 @@
 package nashtech.phucldh.ecommerce.reponsitory;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import nashtech.phucldh.ecommerce.entity.Relationship_Category;
 @Repository
 public interface RelationshipCategoryRepository extends JpaRepository<Relationship_Category, String>{
 
+	Optional<Relationship_Category> findByCategoryid1OrCategoryid2(String categoryid1, String categoryid2);
 }
