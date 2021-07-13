b8fd56b82cfb383dac0d795eed06d72b446dbdd2
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-13 21:30:03 +0700

    Create ResponseDTO.java

---
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/dto/ResponseDTO.java
@@ -0,0 +1,18 @@
+package nashtech.phucldh.ecommerce.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class ResponseDTO {
+	
+	private String errorCode;
+	
+	private Object data;
+	
+	private String successCode;
+	
+}
