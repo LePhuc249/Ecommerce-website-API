68f2d4ce37b241e6219a64dfacd9e2ae74825a74
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-14 11:01:53 +0700

    Update AccountDTO.java

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/dto/AccountDTO.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/dto/AccountDTO.java
@@ -4,15 +4,6 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class AccountDTO {
 
 	private Integer id;
@@ -28,5 +19,81 @@ public class AccountDTO {
 	private LocalDateTime createdate;
 
 	private LocalDateTime updatedate;
-	
+
+	public AccountDTO() {
+	}
+
+	public AccountDTO(Integer id, String username, String fullname, Integer status, Set<String> roles,
+			LocalDateTime createdate, LocalDateTime updatedate) {
+		this.id = id;
+		this.username = username;
+		this.fullname = fullname;
+		this.status = status;
+		this.roles = roles;
+		this.createdate = createdate;
+		this.updatedate = updatedate;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Set<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<String> roles) {
+		this.roles = roles;
+	}
+
+	public LocalDateTime getCreatedate() {
+		return createdate;
+	}
+
+	public void setCreatedate(LocalDateTime createdate) {
+		this.createdate = createdate;
+	}
+
+	public LocalDateTime getUpdatedate() {
+		return updatedate;
+	}
+
+	public void setUpdatedate(LocalDateTime updatedate) {
+		this.updatedate = updatedate;
+	}
+
+	@Override
+	public String toString() {
+		return "AccountDTO [id=" + id + ", username=" + username + ", fullname=" + fullname + ", status=" + status
+				+ ", roles=" + roles + ", createdate=" + createdate + ", updatedate=" + updatedate + "]";
+	}
+
 }
