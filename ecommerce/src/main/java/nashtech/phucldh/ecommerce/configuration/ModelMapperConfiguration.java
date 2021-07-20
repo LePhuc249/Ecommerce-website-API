1193d47a431b0dcb634fc9b854d5d7c0f27291d8
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-20 11:25:54 +0700

    Update ModelMapperConfiguration.java

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/configuration/ModelMapperConfiguration.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/configuration/ModelMapperConfiguration.java
@@ -2,23 +2,17 @@ package nashtech.phucldh.ecommerce.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModelMapperConfiguration {
 
-	@Bean
-	public ModelMapper modelMapper() {
-		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-		return modelMapper;
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
 
-	@Bean
-	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
-		return factory -> factory.setContextPath("/grocerystore/api");
-	}
 }
