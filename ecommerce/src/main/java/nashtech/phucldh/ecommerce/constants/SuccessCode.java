70f2baf2e41f80fd9d196adffd59bed8d2c80ded
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-21 15:45:57 +0700

    Success code Change

---
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/constants/SuccessCode.java
@@ -0,0 +1,62 @@
+package nashtech.phucldh.ecommerce.constants;
+
+public class SuccessCode {
+
+    // User AUTH
+    public static final String USER_SIGNUP_SUCCESS = "USER_SIGNUP_SUCCESS";
+    public static final String USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+    public static final String USER_CREATE_SUCCESS = "USER_CREATE_SUCCESS";
+
+    // Account
+    public static final String ACCOUNT_DELETE_SUCCESS = "ACCOUNT_DELETE_SUCCESS";
+    public static final String ACCOUNT_ACTIVE_SUCCESS = "ACCOUNT_ACTIVE_SUCCESS";
+    public static final String ACCOUNT_UPDATE_SUCCESS = "ACCOUNT_UPDATE_SUCCESS";
+    public static final String ACCOUNT_LOADED_SUCCESS = "ACCOUNT_LOADED_SUCCESS";
+
+    // Organization
+    public static final String ORGANIZATION_CREATE_SUCCESS = "ORGANIZATION_CREATE_SUCCESS";
+    public static final String ORGANIZATION_UPDATE_SUCCESS = "ORGANIZATION_UPDATE_SUCCESS";
+    public static final String ORGANIZATION_DELETE_SUCCESS = "ORGANIZATION_DELETE_SUCCESS";
+    public static final String ORGANIZATION_UNDELETE_SUCCESS = "ORGANIZATION_UNDELETE_SUCCESS";
+    public static final String GET_ORGANIZATION_SUCCESS = "GET_ORGANIZATION_SUCCESS";
+    public static final String GET_ALL_ORGANIZATION_SUCCESS = "GET_ALL_ORGANIZATION_SUCCESS";
+
+    // Brand
+    public static final String BRAND_CREATE_SUCCESS = "BRAND_CREATE_SUCCESS";
+    public static final String BRAND_UPDATE_SUCCESS = "BRAND_UPDATE_SUCCESS";
+    public static final String BRAND_DELETE_SUCCESS = "BRAND_DELETE_SUCCESS";
+    public static final String GET_BRAND_SUCCESS = "GET_BRAND_SUCCESS";
+    public static final String GET_ALL_BRAND_SUCCESS = "GET_ALL_BRAND_SUCCESS";
+
+    // Category
+    public static final String CATEGORY_CREATE_SUCCESS = "CATEGORY_CREATE_SUCCESS";
+    public static final String CATEGORY_UPDATE_SUCCESS = "CATEGORY_UPDATE_SUCCESS";
+    public static final String CATEGORY_DELETE_SUCCESS = "CATEGORY_DELETE_SUCCESS";
+    public static final String CATEGORY_UNDELETE_SUCCESS = "CATEGORY_UNDELETE_SUCCESS";
+    public static final String GET_CATEGORY_SUCCESS = "GET_CATEGORY_SUCCESS";
+    public static final String GET_ALL_CATEGORY_SUCCESS = "GET_ALL_CATEGORY_SUCCESS";
+
+    // Organization Address
+    public static final String ORGANIZATION_ADDRESS_CREATE_SUCCESS = "ORGANIZATION_ADDRESS_CREATE_SUCCESS";
+    public static final String ORGANIZATION_ADDRESS_UPDATE_SUCCESS = "ORGANIZATION_ADDRESS_UPDATE_SUCCESS";
+    public static final String ORGANIZATION_ADDRESS_DELETE_SUCCESS = "ORGANIZATION_ADDRESS_DELETE_SUCCESS";
+    public static final String GET_ORGANIZATION_ADDRESS_SUCCESS = "GET_ORGANIZATION_ADDRESS_SUCCESS";
+    public static final String GET_ALL_ORGANIZATION_ADDRESS_SUCCESS = "GET_ALL_ORGANIZATION_ADDRESS_SUCCESS";
+
+    // Coupons
+    public static final String COUPONS_CREATE_SUCCESS = "COUPONS_CREATE_SUCCESS";
+    public static final String COUPONS_UPDATE_SUCCESS = "COUPONS_UPDATE_SUCCESS";
+    public static final String COUPONS_DELETE_SUCCESS = "COUPONS_DELETE_SUCCESS";
+    public static final String COUPONS_UNDELETE_SUCCESS = "COUPONS_UNDELETE_SUCCESS";
+    public static final String GET_COUPONS_SUCCESS = "GET_COUPONS_SUCCESS";
+    public static final String GET_ALL_COUPONS_SUCCESS = "GET_ALL_COUPONS_SUCCESS";
+
+    // Product
+    public static final String PRODUCT_CREATE_SUCCESS = "PRODUCT_CREATE_SUCCESS";
+    public static final String PRODUCT_UPDATE_SUCCESS = "PRODUCT_UPDATE_SUCCESS";
+    public static final String PRODUCT_DELETE_SUCCESS = "PRODUCT_DELETE_SUCCESS";
+    public static final String PRODUCT_UNDELETE_SUCCESS = "PRODUCT_UNDELETE_SUCCESS";
+    public static final String GET_PRODUCT_SUCCESS = "GET_PRODUCT_SUCCESS";
+    public static final String GET_ALL_PRODUCT_SUCCESS = "GET_ALL_PRODUCT_SUCCESS";
+
+}
