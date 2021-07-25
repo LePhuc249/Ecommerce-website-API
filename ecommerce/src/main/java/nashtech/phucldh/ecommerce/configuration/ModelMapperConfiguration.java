56c48a4d5c6a7f6cf53047842a1198bec8a76df5
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-25 08:55:02 +0700

    Update ModelMapperConfiguration.java

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/configuration/ModelMapperConfiguration.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/configuration/ModelMapperConfiguration.java
@@ -1,7 +1,9 @@
 package nashtech.phucldh.ecommerce.configuration;
 
 import org.modelmapper.ModelMapper;
+
 import org.modelmapper.convention.MatchingStrategies;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +13,7 @@ public class ModelMapperConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
         return modelMapper;
     }
 
