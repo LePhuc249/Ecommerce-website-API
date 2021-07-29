dcd96371e38744dcc2e2e5017c6b5d8ad1beb660
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-29 12:51:13 +0700

    Update Exception

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/exception/GlobalExceptionHandler.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/exception/GlobalExceptionHandler.java
@@ -1,10 +1,8 @@
 package nashtech.phucldh.ecommerce.exception;
 
 import java.time.LocalDateTime;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/exception/UpdateDataFailException.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/exception/UpdateDataFailException.java
@@ -1,7 +1,6 @@
 package nashtech.phucldh.ecommerce.exception;
 
 import org.springframework.http.HttpStatus;
-
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
