a479522e87968fe085c937b646a4c03dae9bf7ca
Le Dac Hoang Phuc <79365285+LePhuc249@users.noreply.github.com>
2021-07-29 12:49:17 +0700

    Update ModelMapperConfiguration.java

--- ecommerce/src/main/java/nashtech/phucldh/ecommerce/configuration/ModelMapperConfiguration.java
+++ ecommerce/src/main/java/nashtech/phucldh/ecommerce/configuration/ModelMapperConfiguration.java
@@ -1,9 +1,7 @@
 package nashtech.phucldh.ecommerce.configuration;
 
 import org.modelmapper.ModelMapper;
-
 import org.modelmapper.convention.MatchingStrategies;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
